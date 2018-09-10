3151ac8c383676feafd46b93499e588dd86fc4c2
Tianyi Xie <tiger.xie@Tigger.local>
2018-09-10 12:12:53 -0400

    TakingInputs

---
+++ TakingInputs.py
@@ -0,0 +1,12 @@
+#Input
+print ('Hola')
+a = input ('¿Tu nombre es?:')
+print ("Sup "+a+"?")
+if a == 'emilio':
+    print ("mista bruuuuuuuce")
+else:
+    print ("Tu nombre no es emilio, fack u")
+#Process
+
+#Output
+print ('Program END')
